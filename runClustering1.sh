0bd5bf60f5b396f4b9df990ca62499bf029f0e3a
RoboHubEindhoven <Robohubeindhoven@gmail.com>
2019-07-31 15:12:54 +0200

    Updated shell scripts

--- runClustering1.sh
+++ runClustering1.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+python face_recognition/faceRecognition.py & disown
 while :
 do
 python encode_faces.py --dataset /home/remco/Projects/face_clustering/dataset --encodings encodings.pickle; echo "Encoding done"
--- runClustering2.sh
+++ runClustering2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+python face_recognition/faceRecognition.py & disown
 while :
 do
 ./face_clustering.py shape_predictor_5_face_landmarks.dat dlib_face_recognition_resnet_model_v1.dat /home/remco/Projects/face_clustering/dataset output_folder; echo "Clustering Done";
