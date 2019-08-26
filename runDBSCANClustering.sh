93aa9c5397b6cd332e5bb6fd6aa12b4871e9a5be
RoboHubEindhoven <Robohubeindhoven@gmail.com>
2019-08-26 16:51:54 +0200

    Deleted DBSCAN

--- runDBSCANClustering.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#python face_recognition/faceRecognition.py & disown
-while :
-do
-python encode_faces.py --dataset /home/remco/Projects/face_clustering/dataset --encodings encodings.pickle; echo "Encoding done"
-python cluster_faces.py --encodings encodings.pickle; echo "Clustering done"
-done
